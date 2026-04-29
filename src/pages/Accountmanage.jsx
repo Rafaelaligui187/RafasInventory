5bcc25b4f74180ab6e2ed7449ee75b35d2f141f0
Rafaelaligui187 <aliguirafael@gmail.com>
2026-04-29 14:22:59 +0800

    Fix account page

--- src/pages/Accountmanage.jsx
+++ src/pages/Accountmanage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-export default function accountmanage() {
+export default function Accountmanage() {
   return (
     <>
     <p>ACCOUNT MANAGEMENT</p>
